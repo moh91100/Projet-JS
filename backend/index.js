a5133b0559a504b76a292ae42e5924923120dc6a
Moh le Boss <aminbrahimi374@gmail.com>
2026-01-30 13:41:23 +0100

    docker avec dossier front et backend

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Backend Node.js fonctionne avec Docker ! 🚀');
+});
+
+app.listen(port, () => {
+  console.log(`Serveur lancé sur le port ${port}`);
+});
